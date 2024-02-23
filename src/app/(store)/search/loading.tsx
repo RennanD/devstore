5c8d1db2f304c8661a1c280d8cde5e866f301fbf
rennand <rplaykl@gmail.com>
2024-02-23 17:14:04 -0300

    chore: useSearchParams fix

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,18 +1,9 @@
-'use client'
-
 import { Skeleton } from '@/components/Skeleton/Skeleton'
-import { useSearchParams } from 'next/navigation'
 
 export default function SearchLoading() {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
-
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados: para <span className="font-semibold">{query}</span>
-      </p>
+      <p className="text-sm">Resultados: para a busca</p>
 
       <div className="grid grid-cols-3 gap-4">
         <Skeleton className="h-[400px]"></Skeleton>

83a4694377ae3da94a40da197cfce07d872bffc0
rennand <rplaykl@gmail.com>
2024-02-23 17:25:02 -0300

    chore: suspense refactory

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,14 +1,12 @@
 import { Header } from '@/components/Header'
 import { CartProvider } from '@/contexts/CartProvider'
-import { ReactNode, Suspense } from 'react'
+import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <CartProvider>
       <div className="mx-auto grid min-h-screen w-full max-w-[78.5rem] grid-rows-[min-content_max-content] gap-6 px-5 pt-10 pb-20">
-        <Suspense>
-          <Header />
-        </Suspense>
+        <Header />
         {children}
       </div>
     </CartProvider>
--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,9 +1,13 @@
 import { Skeleton } from '@/components/Skeleton/Skeleton'
+import { Suspense } from 'react'
+import { CurrentSearch } from './CurrentSearch'
 
 export default function SearchLoading() {
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">Resultados: para a busca</p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-4">
         <Skeleton className="h-[400px]"></Skeleton>
